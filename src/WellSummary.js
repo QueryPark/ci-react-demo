66ae1e2af82b2ee1ed761a835db62f9e0f0f325d
Philippe Larochelle <phil@emiphil.com>
2018-01-16 12:11:24 -0700

    Add qp-well-parser

--- src/WellSummary.js
+++ src/WellSummary.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { flow, map, filter, toPairs } from 'lodash/fp'
 
+import WellParser from 'qp-well-parser'
+
 import './WellSummary.css'
 
-const convertWellDataToRows = (well) => {
+const convertWellDataToRows = (parseWell, well) => {
+  const wellParser = WellParser('v1')
   const hasValue = ([key, value]) => value
   const renderRowFromData = ([key, value]) => {
     if (typeof value === 'object') {
@@ -23,6 +26,7 @@ const convertWellDataToRows = (well) => {
   }
 
   const workflow = flow(
+    (well) => parseWell ? wellParser(well) : well,
     toPairs,
     filter(hasValue),
     map(renderRowFromData)
@@ -31,30 +35,57 @@ const convertWellDataToRows = (well) => {
   return workflow(well)
 }
 
-const WellSummary = ({ well, reset }) => {
-  const { UWI, WellName } = well
+class WellSummary extends Component {
+  constructor (props) {
+    super(props)
 
-  return (
-    <div className='WellSummary-Wrap'
-      onClick={() => reset()}
-    >
-      <section className='WellSummary'>
-        <h1>{WellName || UWI}</h1>
-        <hr />
-        <div className='WellSummary-TableWrap'>
-          <table className='WellSummary-Table'>
-            <tr><th>Key</th><th>Value</th></tr>
-            {convertWellDataToRows(well)}
-          </table>
-        </div>
-        <button className='WellSummary-Button'
-          onClick={() => reset()}
-        >
-          Reset
-        </button>
-      </section>
-    </div>
-  )
+    this.state = {
+      parseWell: true
+    }
+
+    this.toggleParseWell = this.toggleParseWell.bind(this)
+  }
+
+  toggleParseWell () {
+    const current = this.state.parseWell
+    this.setState({ parseWell: !current })
+  }
+
+  render () {
+    const { well, reset } = this.props
+    const { parseWell } = this.state
+    const { UWI, WellName } = well
+
+    return (
+      <div className='WellSummary-Wrap'>
+        <section className='WellSummary'>
+          <header className='WellSummary-Header'>
+            <h1>{WellName || UWI}</h1>
+            <div>
+              <label>
+                <input type='checkbox' checked={parseWell}
+                  onChange={this.toggleParseWell}
+                />
+                QP Parser
+              </label>
+            </div>
+          </header>
+          <hr />
+          <div className='WellSummary-TableWrap'>
+            <table className='WellSummary-Table'>
+              <tr><th>Key</th><th>Value</th></tr>
+              {convertWellDataToRows(parseWell, well)}
+            </table>
+          </div>
+          <button className='WellSummary-Button'
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+        </section>
+      </div>
+    )
+  }
 }
 
 WellSummary.propTypes = {
